83d6002d6fae3986c494f939af2358c1db5a7e5a
Farhan Mahmood <farhanmahmoodny@gmail.com>
2019-01-17 04:43:16 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,9 +1,10 @@
-new_hash = {}
 
 def reformat_languages(languages)
+  new_hash = {}
   languages.each do |style, lang|
     lang.each do |name, type|
       new_hash[name] = type
     end
   end
+  new_hash
 end
