e2e8b5b994ec7f9b3e9a445096a6d5d873641573
Farhan Mahmood <farhanmahmoodny@gmail.com>
2019-01-17 04:58:51 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -6,16 +6,12 @@ def reformat_languages(languages)
     lang.each do |name, type|
       new_hash[name] = type
     end
-    new_hash[:ruby][:style] = []
-    new_hash[:javascript][:style] = lang_keys
-    new_hash[:ruby][:style] = []
-    new_hash[:ruby][:style] = []
-    new_hash[:ruby][:style] = []
-    new_hash[:ruby][:style] = []
-    new_hash[:ruby][:style] = []
+    new_hash.each do |key, value|
+      key[:style] = []
+    end
     new_hash[:ruby][:style].push(lang_keys[0])
-    new_hash[:pyphone]
-    
+    new_hash[:python][:style].push(lang_keys[0])
+    new_hash[:java][:style].push(lang_keys[0])
   end
   new_hash
 end
